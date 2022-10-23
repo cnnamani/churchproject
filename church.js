e6de7f8484d0835e407b0476645994535e004a1c
Chinedu <cnnamani78@gmail.com>
2022-10-23 16:42:56 -0400

    Added footer and changed the background image

--- church.js
+++ church.js
@@ -1,8 +1,8 @@
 const quotes = {
-    love1:"We love because he first loved us.",
-    love2:"Jesus loves you",
-    love3:"Give thanks to the Lord for He is good",
-    love4:"Hatred stirs up strife,But love covers all sin"
+    love1:'"We love because he first loved us."',
+    love2:'"Jesus loves you"',
+    love3:'"Give thanks to the Lord for He is good"',
+    love4:'"Hatred stirs up strife,But love covers all sin"'
 }
     
 
